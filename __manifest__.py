ff707c0a0ee5d9e13b91918e4ef4c48c99b62920
Elhadji Malang DIEDHIOU <supermalang@outlook.com>
2018-05-10 18:24:49 +0000

    Corrections

--- __manifest__.py
+++ __manifest__.py
@@ -1,17 +1,14 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "custom_pos_receipt",
-    'summary': """Personnalisation du ticket de caisse""",
+    'name': 'custom_pos_receipt',
+    'summary': 'Personnalisation du ticket de caisse',
     'version': '10.0.1.1',
-    'description': """Personnalisation du ticket de caisse""",
-    'author': "Elhadji Malang Diedhiou",
-    'website': "http://www.supermalang.com",
+    'description': 'Personnalisation du ticket de caisse',
+    'author': 'Elhadji Malang Diedhiou',
+    'website': 'http://www.supermalang.com',
     'category': 'Point Of Sale',
     'depends': ['point_of_sale'],
     'data': [],
-    'demo': [],
     'qweb': ['static/src/xml/pos_ticket.xml'],
     'application':True,
-    'installable': True,
-    'auto_install': False,
 }
