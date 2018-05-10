68ffba11ffa20eab696651c89ddfa9d9a5ee9d07
Elhadji Malang DIEDHIOU <supermalang@outlook.com>
2018-05-10 16:08:35 +0000

    Corrections

--- __manifest__.py
+++ __manifest__.py
@@ -1,35 +1,16 @@
 # -*- coding: utf-8 -*-
 {
     'name': "custom_pos_receipt",
-
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
-
-    'description': """
-        Long description of module's purpose
-    """,
-
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
-    'depends': ['base'],
-
-    # always loaded
-    'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    'summary': """Personnalisation du ticket de caisse""",
+    'version': '10.0.1.1',
+    'description': """Personnalisation du ticket de caisse""",
+    'author': "Elhadji Malang Diedhiou",
+    'website': "http://www.supermalang.com",
+    'category': 'Point Of Sale',
+    'depends': ['base','point_of_sale'],
+    'data': [],
+    'demo': [],
+    'qweb': ['static/src/xml/pos_ticket.xml'],
+    'installable': True,
+    'auto_install': False,
 }
